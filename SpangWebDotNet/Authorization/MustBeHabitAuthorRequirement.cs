9346959a962727b09314195e48dd7ff305385921
Pete Spangler <petertspangler@gmail.com>
2021-02-19 21:46:11 -0800

    added react solution

---
+++ SpangWebDotNet/Authorization/MustBeHabitAuthorRequirement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace SpangWebDotNet.Authorization
+{
+    public class MustBeHabitAuthorRequirement:IAuthorizationRequirement
+    {
+        public MustBeHabitAuthorRequirement()
+        {
+
+        }
+    }
+}
--- SpangWebDotNet/Startup.cs
+++ SpangWebDotNet/Startup.cs
@@ -1,15 +1,23 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using SpangWebDotNet.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
 using DbUp;
+using SpangWebDotNet.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
+using SpangWebDotNet.Authorization;
 
 namespace SpangWebDotNet
 {
@@ -40,6 +48,33 @@ namespace SpangWebDotNet
             {
                 upgrader.PerformUpgrade();
             }
+            services.AddScoped<IDataRepository, DataRepository>();
+            services.AddMemoryCache();
+            services.AddSingleton<IHabitCache, HabitCache>();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme =
+                  JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme =
+                  JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.Authority = Configuration["Auth0:Authority"];
+                options.Audience = Configuration["Auth0:Audience"];
+            });
+            services.AddHttpClient();
+            services.AddAuthorization(options =>
+                  options.AddPolicy("MustBeQuestionAuthor", policy =>
+                    policy.Requirements.Add(new MustBeHabitAuthorRequirement())));
+            services.AddScoped<IAuthorizationHandler, MustBeHabitAuthorHandler>();
+            services.AddHttpContextAccessor();
+
+            services.AddCors(options => options.AddPolicy("CorsPolicy", builder =>
+                  builder
+                   .AllowAnyMethod()
+                   .AllowAnyHeader()
+                   .WithOrigins(Configuration["Frontend"])));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -59,7 +94,8 @@ namespace SpangWebDotNet
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseCors("CorsPolicy");
+            app.UseAuthorization();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
